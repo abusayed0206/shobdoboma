c85a214188bbc77f9d9bc3178c0143d36f2c0af9
MD. ABU SAYED <hello@sayed.page>
2024-08-15 15:22:52 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'বাংলা শব্দবোমা,
+  description: 'যেখানে শব্দবোমা ফাটানো হয়।',
 }
 
 export default function RootLayout({
